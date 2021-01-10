252abcea6f8d46c03edc49ed325b59a0b51631f0
RobsondCCarneiro <robinho.cenep@gmail.com>
2021-01-10 01:47:17 -0300

    POST /orders

--- backend/src/main/java/com/devsuperior/dsdeliver/services/OrderService.java
+++ backend/src/main/java/com/devsuperior/dsdeliver/services/OrderService.java
@@ -1,5 +1,6 @@
 package com.devsuperior.dsdeliver.services;
 
+import java.time.Instant;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -8,8 +9,12 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.devsuperior.dsdeliver.dto.OrderDTO;
+import com.devsuperior.dsdeliver.dto.ProductDTO;
 import com.devsuperior.dsdeliver.entities.Order;
+import com.devsuperior.dsdeliver.entities.OrderStatus;
+import com.devsuperior.dsdeliver.entities.Product;
 import com.devsuperior.dsdeliver.repositories.OrderRespository;
+import com.devsuperior.dsdeliver.repositories.ProductRespository;
 
 @Service
 public class OrderService {
@@ -17,9 +22,34 @@ public class OrderService {
 	@Autowired
 	private OrderRespository repository;
 	
+	//Aqui serve para acessar os produtos que cada pedido estah associado.
+	@Autowired
+	private ProductRespository productRepository;
+	
 	@Transactional(readOnly = true)
 	public List<OrderDTO> findAll(){
 		List<Order> list = repository.findOrdersWithProducts();
 		return list.stream().map(x -> new OrderDTO(x)).collect(Collectors.toList());
+	}
+	
+	@Transactional
+	public OrderDTO insert(OrderDTO dto){
+		Order order = new Order(null, dto.getAddress(), dto.getLatitude(), dto.getLongitude(),
+				Instant.now(), OrderStatus.PENDING);
+		
+		/*
+		 * Para associar os pedidos que jah estavam armazenados no Banco de Dados com os novos,
+		 * eh preciso fazer esse laco aqui. O metodo POST eh para inserir arquivos no BD.
+		 * NAO PODE ASSOCIAR DTO COM ENTIDADE, TEM QUE INSTANCIAR UMA ENTIDADE PARA CADA
+		 * ProductDTO.
+		 */
+		for(ProductDTO p : dto.getProducts()) {
+			//O getOne instancia um produto sem acessar o BD. SALVA AS ASSOCIACOES QUE CADA PRODUTO TEM AO SEU PEDIDO
+			Product product = productRepository.getOne(p.getId());
+			order.getProducts().add(product);
+		}
+		
+		order = repository.save(order);
+		return new OrderDTO(order);
 	}
 }
